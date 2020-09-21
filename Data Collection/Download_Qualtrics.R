5070c89bb3d9c5fc65688024d31832a1ffa56d7e
katherineathom <kateathom@gmail.com>
2020-09-21 10:08:35 -0400

    update argument name to fix ?

--- Data Collection/Download_Qualtrics.R
+++ Data Collection/Download_Qualtrics.R
@@ -25,10 +25,12 @@ qualtrics_api_credentials(
 surveys <- all_surveys()
 
 # Function for reading in and saving the survey to the Y 
-get_forms <- function(name, location) {
+get_forms <- function(surveyname, location) {
   
+  ## Add something that only downloads it if last modified recently 
+  ## and is currently active 
   id = surveys %>%
-    filter(name == name) %>%
+    filter(name == surveyname) %>%
     .$id
   
   survey <- fetch_survey(surveyID = id, 
@@ -36,7 +38,7 @@ get_forms <- function(name, location) {
   
   today <- as.character(Sys.Date())
  
-  filepath <- paste0("Y:/LHP/FUP/Impact Study/RData/Qualtrics/", location, today, ".csv")
+  filepath <- paste0("Y:/LHP/FUP/Impact Study/RData/Qualtrics/",location, today, ".csv")
   write.csv(survey, filepath)
   
   print(paste0("file saved to ", filepath, "!!!!"))
@@ -44,10 +46,10 @@ get_forms <- function(name, location) {
 }
 
 # Do the thing 
-get_forms(name = "Housing Application and Search Assistance Questionnaire - PHX", location = "PHX_HAQ/PHX_HAQ")
-get_forms(name = "FUP Housing Status Form - PHX", location = "PHX_HSF/PHX_HSF")
-get_forms(name = "Ongoing Services Questionnaire - OC", location = "OC_OSQ/OC_OSQ")
-get_forms(name = "Housing Application and Search Assistance Questionnaire - OC", location = "OC_HAQ/OC_HAQ")
+get_forms(surveyname = "Housing Application and Search Assistance Questionnaire - PHX", location = "PHX_HAQ/PHX_HAQ")
+get_forms(surveyname = "FUP Housing Status Form - PHX", location = "PHX_HSF/PHX_HSF")
+get_forms(surveyname = "Ongoing Services Questionnaire - OC", location = "OC_OSQ/OC_OSQ")
+get_forms(surveyname = "Housing Application and Search Assistance Questionnaire - OC", location = "OC_HAQ/OC_HAQ")
 
 
 
